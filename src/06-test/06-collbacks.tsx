86f7f4e68b1431930be8199a0e11b0a56afca924
Wlad loban <qwegadqwe@gmail.com>
2022-05-30 21:25:26 +0300

    06-callback

---
+++ src/06-test/06-collbacks.tsx
@@ -0,0 +1,29 @@
+import {ChangeEvent} from 'react';
+
+export const User = () => {
+
+    const deleteUser = () => {
+        alert('user have benn deleted')
+
+    }
+    const saveUser = () => {
+        alert('user have benn saved')
+    }
+
+    const onNameChanged=()=>{
+        console.log('name changed')
+    }
+    const focusLostHandler=()=>{
+        console.log('focus lost')
+    }
+    const onAgeChanged=(event:ChangeEvent<HTMLInputElement>)=>{
+        console.log('age changed'+event.currentTarget.value)
+    }
+    return (
+        <div>
+            <textarea onChange={onNameChanged} onBlur={focusLostHandler}>Dimych</textarea>
+            <input onChange={onAgeChanged} type={'number'}/>
+            <button onClick={deleteUser}>delete</button>
+            <button onClick={saveUser}>save</button>
+        </div>)
+}
